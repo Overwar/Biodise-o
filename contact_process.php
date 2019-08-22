a6efe305508d3ac924bb1c1ad0eaf83db56811f7
Overwar <43070236+Overwar@users.noreply.github.com>
2019-08-22 18:59:00 -0500

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "a20183307@pucp.edu.pe";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
